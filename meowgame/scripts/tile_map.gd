eaae92bdc7fb560c8b40c2dd94e32cf56b4b36a7
morgantome <153690590+morgantome@users.noreply.github.com>
2025-04-29 18:38:26 -0400

    index on develop: f15bc5b add sprites and some other stuff idk

--- meowgame/scripts/tile_map.gd
+++ meowgame/scripts/tile_map.gd
@@ -4,12 +4,23 @@ extends TileMap
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var mouse_pos = get_global_mouse_position()
+		var tile_pos = local_to_map(mouse_pos)
+		
+		# If using a pickaxe, try to remove a tile.
 		if inventory.is_equipped("pickaxe"):
-			var mouse_pos = get_global_mouse_position()
-			var tile_pos = local_to_map(mouse_pos)
-
 			if get_cell_atlas_coords(0, tile_pos) != Vector2i(-1, -1):
 				erase_cell(0, tile_pos)
 				print("Tile destroyed!")
-		else:
-			print("You need a pickaxe to break tiles!")
+			else:
+				print("No tile to destroy at this location!")
+		
+		# If using a block, try to add a tile.
+		elif inventory.is_equipped("block"):
+			# Check if the target position has no tile.
+			if get_cell_atlas_coords(0, tile_pos) == Vector2i(-1, -1):
+				# Set the cell to have tile index 0. Change the index as needed.
+				set_cell(0, tile_pos)
+				print("Tile added!")
+			else:
+				print("There's already a tile here!")
